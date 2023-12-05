66a82a19166396f3d566ed3c155649058a4de315
Cesar Herrera <cesarherr321@gmail.com>
2023-12-05 09:41:35 -0300

    Add Comment migration file
    Co-authored-by: Ruthmy17 <ruthmy17@gmail.com>

---
+++ db/migrate/20231205123556_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[7.1]
+  def change
+    create_table :comments do |t|
+      t.text :comment, null:false, limit: 1000
+      t.integer :book_id, null:false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
