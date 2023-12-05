c1a0f3ad142f80df29276650ca5c1ab6b8fcfa90
Cesar Herrera <cesarherr321@gmail.com>
2023-12-05 09:27:18 -0300

    Add User migration
    Co-authored-by: Ruthmy17 <ruthmy17@gmail.com>

---
+++ db/migrate/20231205122325_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null:false, limit: 50
+      t.string :username, null:false, limit: 50
+      t.string :avatar
+      t.string :bio, null:false, limit: 160
+      t.string :country, null:false, limit: 100
+
+      t.timestamps
+    end
+  end
+end
