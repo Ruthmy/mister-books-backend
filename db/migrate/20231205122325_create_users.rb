a8ec2176569e7eb09d8b8e55fe0f4083512d4f20
Ruthmy <ruthmy17@gmail.com>
2023-12-05 15:20:36 -0600

    Modified 'create_users' -migration- - Remove the uniqueness on username
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

--- db/migrate/20231205122325_create_users.rb
+++ db/migrate/20231205122325_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name, null:false, limit: 50
-      t.string :username, null:false, limit: 50, unique: true
+      t.string :username, null:false, limit: 50
       t.string :avatar
       t.string :bio, null:false, limit: 160
       t.string :country, null:false, limit: 100
