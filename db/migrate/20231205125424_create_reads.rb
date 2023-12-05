fb159213cd4f95b87fd5bfd2d5b1bcf26cc2ced7
Ruthmy <ruthmy17@gmail.com>
2023-12-05 16:00:45 -0600

    Created 'create_reads' -migration-
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

---
+++ db/migrate/20231205125424_create_reads.rb
@@ -0,0 +1,10 @@
+class CreateReads < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reads do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :book_id, null: false
+
+      t.timestamps
+    end
+  end
+end
