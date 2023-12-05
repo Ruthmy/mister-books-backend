beee4c49ea9f4774cee6df62aab1830b663857b0
Ruthmy <ruthmy17@gmail.com>
2023-12-05 15:56:21 -0600

    Modified 'create_comments' -migration- - Added a counter like.
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

--- db/migrate/20231205123556_create_comments.rb
+++ db/migrate/20231205123556_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
+      t.integer :counter_like, null:false, default: 0
       t.text :comment, null:false, limit: 1000
       t.integer :book_id, null:false
       t.references :user, null: false, foreign_key: true
