9bbeb35be1d42a1f930be044d6f12d9e0d3e3977
Ruthmy <ruthmy17@gmail.com>
2023-12-05 20:56:45 -0600

    Modified  'comment.rb' -model-
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  has_many :like_comments, dependent: :destroy
+
+  validates :comment, :book_id, :counter_like, presence: true
+  validates :comment, length: { maximum: 500 }
 end
