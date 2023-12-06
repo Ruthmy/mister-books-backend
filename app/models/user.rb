ff983f9d892d2fbfd42f420e094e758e064cac7c
Ruthmy <ruthmy17@gmail.com>
2023-12-05 20:56:36 -0600

    Modified 'user.rb' -model-
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :like_comments, dependent: :destroy
+  has_many :read, dependent: :destroy
+  has_many :wishlists, dependent: :destroy
+  has_many :ratings, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :name, :avatar, :bio, :country, presence: true
+  validates :name, length: { maximum: 50 }
+  validates :bio, length: { maximum: 160 }
+  validates :country, length: { maximum: 100 }
+  # Pendiente de agregar Avatar
 end
