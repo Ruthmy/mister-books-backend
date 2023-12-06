c4a5b414c13464eb3dedba48b1d69414f47f0bff
Ruthmy <ruthmy17@gmail.com>
2023-12-05 20:57:13 -0600

    Modified  'rating.rb' -model-
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,6 @@
 class Rating < ApplicationRecord
+  belongs_to :user
+
+  validates :rating, :book_id, :user_id, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
