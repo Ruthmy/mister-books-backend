bfb02f5e40455dfddd7d6f9728dfbcde7630080d
Ruthmy <ruthmy17@gmail.com>
2023-12-08 08:04:41 -0600

    Modified 'schema.rb' -file- - Updated the schema db.
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -54,8 +54,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_08_122033) do
     t.string "name", limit: 50, null: false
     t.string "username", limit: 50, null: false
     t.string "avatar"
-    t.string "bio", limit: 160, null: false
-    t.string "country", limit: 100, null: false
+    t.string "bio", limit: 160
+    t.string "country", limit: 100
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
