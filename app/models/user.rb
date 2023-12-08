c2ebc5606c7417cb7a29f8edc1905d03df40a879
Ruthmy <ruthmy17@gmail.com>
2023-12-08 08:04:01 -0600

    Modified user.rb' -model- - Remove the presence condition for avatar, bio and country.
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :ratings, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
-  validates :name, :avatar, :bio, :country, presence: true
+  validates :name, presence: true
   validates :name, length: { maximum: 50 }
   validates :bio, length: { maximum: 160 }
   validates :country, length: { maximum: 100 }
