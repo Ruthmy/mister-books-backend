28998c55ce3a365ed9ee4911525878b30b9b1e6f
Ruthmy <ruthmy17@gmail.com>
2023-12-08 08:03:29 -0600

    Modified 'create_users' -migration- - Remove the null condition from bio and country.
    Co-authored-by: CesarHerr <cesarherr321@gmail.com>

--- db/migrate/20231205122325_create_users.rb
+++ db/migrate/20231205122325_create_users.rb
@@ -4,8 +4,8 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :name, null:false, limit: 50
       t.string :username, null:false, limit: 50
       t.string :avatar
-      t.string :bio, null:false, limit: 160
-      t.string :country, null:false, limit: 100
+      t.string :bio, limit: 160
+      t.string :country, limit: 100
 
       t.timestamps
     end
