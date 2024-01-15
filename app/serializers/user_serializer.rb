5f37976e0de952ef658313a574e905318dd34736
Cesar Herrera <cesarherr321@gmail.com>
2024-01-15 00:08:28 -0300

    Add registration and session configuration

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :id, :name, :username, :email, :created_at
+
+  attribute :created_date do |user|
+    user.created_at&.strftime('%m/%d/%Y')
+  end
 end
