aa28ba95335ffd4c0d03d09dd3d0899f73f8125e
Cesar Herrera <cesarherr321@gmail.com>
2024-01-14 23:59:32 -0300

    Add a revocation strategy to devise-jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :comments, dependent: :destroy
   has_many :like_comments, dependent: :destroy
