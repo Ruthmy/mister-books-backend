a4b05fbd7200293f1b4148b9e1d1aa3aac2d165a
Cesar Herrera <cesarherr321@gmail.com>
2024-01-19 00:09:02 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :users, path: '', path_names: {
@@ -14,7 +20,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root to: 'api/v1/users#index'
 
   # Defines the root path route ("/")
   # root "posts#index"
