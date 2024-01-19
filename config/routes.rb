bc7bff9e7c2a7356b0f26a5ec5748b20b00142a5
Cesar Herrera <cesarherr321@gmail.com>
2024-01-19 00:47:39 -0300

    Add current user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/current_user', to: 'current_user#index'
   namespace :api do
     namespace :v1 do
       resources :users
@@ -21,7 +22,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root to: 'api/v1/users#index'
-
   # Defines the root path route ("/")
   # root "posts#index"
 end
