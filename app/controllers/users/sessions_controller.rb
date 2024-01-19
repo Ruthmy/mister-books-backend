edfd3847b214a655b256f36697824d150aa488a0
Cesar Herrera <cesarherr321@gmail.com>
2024-01-19 00:07:53 -0300

    Refactor session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,17 +4,24 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: {
-      status: { code: 200, message: 'Logged in sucessfully.' },
-      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-    }, status: :ok
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: 'Logged in successfully.' },
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: 'Invalid Email or password.'
+      }, status: :unauthorized
+    end
   end
 
   def respond_to_on_destroy
     if current_user
       render json: {
         status: 200,
-        message: 'logged out successfully'
+        message: 'Logged out successfully.'
       }, status: :ok
     else
       render json: {
