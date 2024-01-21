e51ac6fe15e160b7f7d8b8ccbbb4bfeab9f76527
Cesar Herrera <cesarherr321@gmail.com>
2024-01-20 23:59:29 -0300

    Add create and destroy actions

---
+++ app/controllers/api/v1/like_comments_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::LikeCommentsController < ApplicationController
+  def create
+    @comment = Comment.find(params[:comment_id])
+    @like = LikeComment.new(comment_id: @comment.id, user_id: current_user.id)
+
+    if @like.save
+      render json: { success: true, message: 'You have liked the comment!.' }
+    else
+      render json: { success: false, message: "Something is wrong!, you don't liked the post!." }
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:comment_id])
+    @like = LikeComment.find_by(comment: @comment, user: current_user)
+
+    if @like.destroy
+      render json: { success: true, message: 'You have unliked the comment!.' }
+    else
+      render json: { success: false, message: "Something is wrong!, you don't unliked the post!." }
+    end
+  end
+end
